7650edb08acc07f50473b951c7afe6fac327d064
海天涯 <ylz192325@163.com>
2017-12-12 10:17:25 +0800

    1
    修改端口，以及提示语

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ func main() {
  router := gin.Default()
 
  router.GET("/", func(c *gin.Context) {
-  c.String(http.StatusOK, "It works")
+  c.String(http.StatusOK, "Hello World!")
  })
 
- router.Run(":8000")
+ router.Run(":8800")
 }
